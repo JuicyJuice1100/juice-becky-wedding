1267d1d3346b5465c2cd1eae6f6af4a92c3fd127
Juice <espiritu.l.justin@gmail.com>
2020-02-08 04:15:24 -0600

    created more components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,29 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationComponent } from './navigation/navigation.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { DetailsComponent } from './details/details.component';
+import { GuestBookComponent } from './guest-book/guest-book.component';
+import { RegistryComponent } from './registry/registry.component';
+import { RsvpComponent } from './rsvp/rsvp.component';
+import { HomeHeaderComponent } from './home-header/home-header.component';
+import { HomePictureCarouselComponent } from './home-picture-carousel/home-picture-carousel.component';
+import { GuestInfoComponent } from './guest-info/guest-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    DetailsComponent,
+    GuestBookComponent,
+    RegistryComponent,
+    RsvpComponent,
+    HomeHeaderComponent,
+    HomePictureCarouselComponent,
+    GuestInfoComponent
   ],
   imports: [
     BrowserModule,
