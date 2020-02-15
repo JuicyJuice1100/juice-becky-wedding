3dbde7aceb2e7ccde87eb3da1875b9da2028f4e4
Juice <espiritu.l.justin@gmail.com>
2020-02-15 12:51:13 -0600

    reorganized components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavigationComponent } from './navigation/navigation.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { DetailsComponent } from './details/details.component';
-import { GuestBookComponent } from './guest-book/guest-book.component';
-import { RegistryComponent } from './registry/registry.component';
-import { RsvpComponent } from './rsvp/rsvp.component';
-import { HomeHeaderComponent } from './home-header/home-header.component';
-import { HomePictureCarouselComponent } from './home-picture-carousel/home-picture-carousel.component';
-import { GuestInfoComponent } from './guest-info/guest-info.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { DetailsComponent } from './components/details/details.component';
+import { GuestBookComponent } from './components/guest-book/guest-book.component';
+import { RegistryComponent } from './components/registry/registry.component';
+import { RsvpComponent } from './components/rsvp/rsvp.component';
+import { HomeHeaderComponent } from './components/home-header/home-header.component';
+import { HomePictureCarouselComponent } from './components/home-picture-carousel/home-picture-carousel.component';
+import { GuestInfoComponent } from './components/guest-info/guest-info.component';
 
 @NgModule({
   declarations: [
