e2c96f38ee4625a7d98d1e2c55d485c294a50e57
Juice <espiritu.l.justin@gmail.com>
2020-02-23 05:14:17 -0600

    fixed some navigation, still cannot figure out Full Page Intro with fixed, transparent Navbar

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -5,11 +5,10 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent implements OnInit {
 
+export class NavigationComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
