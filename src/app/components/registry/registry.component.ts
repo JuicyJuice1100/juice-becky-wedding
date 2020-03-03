39d63140521430bca5e1ceed9d839e2adf9aaebb
Juice <espiritu.l.justin@gmail.com>
2020-03-02 19:36:13 -0600

    registry area templated

--- src/app/components/registry/registry.component.ts
+++ src/app/components/registry/registry.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-registry',
-  templateUrl: './registry.component.html',
-  styleUrls: ['./registry.component.scss']
+  selector: "app-registry",
+  templateUrl: "./registry.component.html",
+  styleUrls: ["./registry.component.scss"]
 })
 export class RegistryComponent implements OnInit {
+  cards = [
+    {
+      title: 'Amazon',
+      url: 'https://www.amazon.com/',
+      image: 'https://picsum.photos/300/500'
+    },
+    {
+      title: 'Target',
+      url: 'https://www.target.com/',
+      image: 'https://picsum.photos/300/500'
+    }
+  ]
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
