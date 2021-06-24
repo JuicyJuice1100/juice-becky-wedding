bdcf1aac6916fd4e272b1cae70d0c1f65684f07b
Juice <espiritu.l.justin@gmail.com>
2021-06-24 11:15:24 -0500

    compressed images

--- src/app/services/image/image.service.ts
+++ src/app/services/image/image.service.ts
@@ -18,15 +18,15 @@ export class ImageService {
 }
 
 const venueImageDetails = [
-  {"id": 1, "url": "../assets/images/venue/1d1ab030-b787-48df-8f86-5333215fb186.jpg", "alt": "", "description": ""},
-  {"id": 2, "url": "../assets/images/venue/FullWidth_Image_1680x860_Sampson_0.jpg", "alt": "", "description": ""},
-  {"id": 3, "url": "../assets/images/venue/FullWidth_Image_1680x860_streets_2.jpg", "alt": "", "description": ""},
-  {"id": 4, "url": "../assets/images/venue/FullWidth_Image_1680x860_torosaur.jpg", "alt": "", "description": ""},
-  {"id": 5, "url": "../assets/images/venue/MPM_0134_GenStore_Interior.jpg", "alt": "", "description": ""},
-  {"id": 6, "url": "../assets/images/venue/MPM_0136_GeneralStore.jpg", "alt": "", "description": ""},
-  {"id": 7, "url": "../assets/images/venue/MPM_0157_Chapman's.jpg", "alt": "", "description": ""},
-  {"id": 8, "url": "../assets/images/venue/MPM_0169_FountainMural.jpg", "alt": "", "description": ""},
-  {"id": 9, "url": "../assets/images/venue/MPM_0185_Granny'sHouse.jpg", "alt": "", "description": ""},
-  {"id": 10, "url": "../assets/images/venue/MPM_0317_PfisterEntrance.jpg", "alt": "", "description": ""},
-  {"id": 11, "url": "../assets/images/venue/MPM_0483_NorthSide.jpg", "alt": "", "description": ""}
+  {"id": 1, "url": "../assets/images/venue/1d1ab030-b787-48df-8f86-5333215fb186-min.jpg", "alt": "", "description": ""},
+  {"id": 2, "url": "../assets/images/venue/FullWidth_Image_1680x860_Sampson_0-min.jpg", "alt": "", "description": ""},
+  {"id": 3, "url": "../assets/images/venue/FullWidth_Image_1680x860_streets_2-min.jpg", "alt": "", "description": ""},
+  {"id": 4, "url": "../assets/images/venue/FullWidth_Image_1680x860_torosaur-min.jpg", "alt": "", "description": ""},
+  {"id": 5, "url": "../assets/images/venue/MPM_0134_GenStore_Interior-min.jpg", "alt": "", "description": ""},
+  {"id": 6, "url": "../assets/images/venue/MPM_0136_GeneralStore-min.jpg", "alt": "", "description": ""},
+  {"id": 7, "url": "../assets/images/venue/MPM_0157_Chapman's-min.jpg", "alt": "", "description": ""},
+  {"id": 8, "url": "../assets/images/venue/MPM_0169_FountainMural-min.jpg", "alt": "", "description": ""},
+  {"id": 9, "url": "../assets/images/venue/MPM_0185_Granny'sHouse-min.jpg", "alt": "", "description": ""},
+  {"id": 10, "url": "../assets/images/venue/MPM_0317_PfisterEntrance-min.jpg", "alt": "", "description": ""},
+  {"id": 11, "url": "../assets/images/venue/MPM_0483_NorthSide-min.jpg", "alt": "", "description": ""}
 ]
