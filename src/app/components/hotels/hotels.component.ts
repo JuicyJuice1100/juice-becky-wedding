44628507133ce0b48334ff1070e46fea76cfb0dc
Juice <espiritu.l.justin@gmail.com>
2021-08-21 23:39:32 -0500

    made some color styling changes

--- src/app/components/hotels/hotels.component.ts
+++ src/app/components/hotels/hotels.component.ts
@@ -11,13 +11,15 @@ export class HotelsComponent implements OnInit {
     {
       title: 'Saint Kate',
       url: 'https://res.saintkatearts.com/ibe/details.aspx?propertyid=16098&nights=2&checkin=05/13/2022&group=051422SEWB',
-      address: "139 E Kilbourn Ave, Milwaukee, WI 53202"
+      address: "139 E Kilbourn Ave, Milwaukee, WI 53202",
+      color: "primary"
     },
     {
       title: 'Homewood Suites',
       //TODO: Get homewood link
       url: '<INSERT LINK HERE>',
-      address: "500 N. Water Street, Milwaukee, WI 53202"
+      address: "500 N. Water Street, Milwaukee, WI 53202",
+      color: "accent"
     }
   ];
 
